7b0fe6fca463d40077e6f279af2b2934273275dd
Jimmy McBride <mcbride967@gmail.com>
2023-07-18 00:14:37 -0500

    fixed bug roll

--- Roll Result Spawn Point.gd
+++ Roll Result Spawn Point.gd
@@ -6,15 +6,15 @@ extends Control
 
 var end_held = false
 
-func _on_attack_button_pressed():
-	spawn_roll_result(randi_range(1,10) + mastery)
-
-func spawn_roll_result(value:float):
-	var roll_result = get_roll_result()
-	var val = str(round(value))
-	var pos = roll_result_spawn_point.position
-	add_child(roll_result, true)
-	roll_result.set_values_and_animate(val, pos)
-
-func get_roll_result() -> roll_result_number:
-	return roll_result_template.instantiate()
+#func _on_attack_button_pressed():
+#	spawn_roll_result(randi_range(1,10) + mastery)
+#
+#func spawn_roll_result(value:float):
+#	var roll_result = get_roll_result()
+#	var val = str(round(value))
+#	var pos = roll_result_spawn_point.position
+#	add_child(roll_result, true)
+#	roll_result.set_values_and_animate(val, pos)
+#
+#func get_roll_result() -> roll_result_number:
+#	return roll_result_template.instantiate()
--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 #Player stuff vvv
 @onready var animation = $AnimationPlayer
+@onready var roll_indicator = $"../Roll Result Indicator" # No need to preload this, it's already a child in the same scene, we can access it directly
+@onready var attack_button = $"../Attack Button"
 var total_health = "res://Class.gd.Priest.total_health"
 var mastery_bonus = 3
 
@@ -11,7 +13,19 @@ func updateAnimation():
 func _physics_process(_delta):
 	move_and_slide()
 	updateAnimation()
+	
+func _on_attack_button_pressed():
+	var result = randi_range(1,10) + mastery_bonus
+	spawn_roll_indicator(result)
+	# This disables the attack button while the animation is playing
+	attack_button.disabled = true
+	await get_tree().create_timer(1.0).timeout
+	attack_button.disabled = false
 
+func spawn_roll_indicator(result: int):
+	if roll_indicator:
+		roll_indicator.get_node("Label").text = str(result)
+		roll_indicator.get_node("AnimationPlayer").play("show_result")
 
 #func spawn_effect(EFFECT: PackedScene, effect_position: Vector2 = global_position):
 #	if EFFECT:
@@ -19,12 +33,6 @@ func _physics_process(_delta):
 #		get_tree().current_scene.add_child(effect)
 #		effect.global_position = effect_position
 #		return effect
-
-#func spawn_roll_indicator(result: int):
-#	var indicator = roll_indicator.instantiate()
-#	if indicator:
-#		indicator.label = (result) #think crash is caused by this. Invalid get index 'label' (on base:PackedScene') 
-
 
 	#get a result from the pool vvv
 #	if roll_result_pool.size() > 0:
