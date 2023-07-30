94ce49e073c8da4620d0bb53eb578c17f3ad1497
Jimmy McBride <mcbride967@gmail.com>
2023-07-29 20:22:40 -0500

    removed orphans

--- Class.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-class_name Priest
-
-#PASSIVE: HANDS OF HEALING
-#When you heal someone with a card ability or receive healing for any reason, one other character of your choice is healed for half of the same amount
--- health component.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-class_name HEALTH
-
-@export_category("Health")
-@export_range(0, 1000, 1) var total_health : int
-
